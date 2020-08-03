ca047f9e75f1c25577ea153f5a1d8bfea0f1d730
roboblox <supeecyborg@gmail.com>
2020-08-03 19:02:15 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
-import React from 'react';
+import React from "react";
+import "./components/Todo.css";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
+const tasks = [
+  {
+    info: "run",
+    id: 1,
+    completed: false,
+  },
+  {
+    info: "walk",
+    id: 2,
+    completed: false,
+  },
+  {
+    info: "swim",
+    id: 3,
+    completed: false,
+  },
+  {
+    info: "fly",
+    id: 4,
+    completed: false,
+  },
+  {
+    info: "sing",
+    id: 5,
+    completed: false,
+  },
+];
 class App extends React.Component {
   // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      tasks, // same as === tasks: tasks
+    };
+  }
+
+  addTask = (e, task) => {
+    e.preventDefault();
+    const newTask = {
+      info: task,
+      id: Date.now(),
+      completed: false,
+    };
+    this.setState(
+      {
+        tasks: [...this.state.tasks, newTask],
+      },
+      () => {
+        localStorage.setItem("tasks", JSON.stringify(this.state.tasks));
+      }
+    );
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1>Todo List</h1>
+          <TodoForm addTask={this.addTask} />
+        </div>
+        <TodoList
+          tasks={this.state.tasks}
+          toggleTask={this.toggleTask}
+          clearCompleted={this.clearCompleted}
+        />
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todo = (props) => {
+  return (
+    <div>
+      <p>{props.task.info}</p>
+    </div>
+  );
+};
+
+export default Todo;
